6d3ee6ed5f57b192237331f86a7f39d22ef2a38d
davidjin586 <smartdev1002@gmail.com>
2024-05-28 19:57:04 -0400

    complete 1 version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import "./App.css";
 
@@ -7,30 +7,128 @@ import { Card, Form, Row, Col, Button } from "react-bootstrap";
 import { useSDK } from "@metamask/sdk-react";
 
 //web3
-import { Web3 } from 'web3';
+import { Web3 } from "web3";
 
+//ABI
+import bridgeAbi from "./bscBridgeContractABI.json";
 function App() {
-  const [account, setAccount] = useState(null);
-  const { sdk, connected, connecting, provider, chainId } = useSDK();
-  const web3 = new Web3('https://rpc.ankr.com/bsc_testnet_chapel/32995e49d45223d3998a076f6633afe2f8d2c43036bad7b2cb6f792ad692a5e9');
+  const [bscTokenContract, setBscTokenContract] = useState(null);
+  const [bscBridgeContract, setBscBridgeContract] = useState(null);
 
-  const connect = useCallback(async () => {
-    try {
-      const accounts = await sdk?.connect();
-      setAccount(accounts?.[0]);
-    } catch (error) {
-      console.warn("failed to connect..", error);
-    }
-  }, [account]);
+  const [hecoTokenContract, setHecoTokenContract] = useState(null);
+  const [hecoBridgeContract, setHecoBridgeContract] = useState(null);
+
+  const account = useRef(null);
+
+  const { sdk, connected, connecting, provider, chainId } = useSDK();
+  // const web3 = new Web3(new Web3.providers.HttpProvider("https://data-seed-prebsc-1-s1.binance.org:8545"));
+  // console.log(window.web3.currentProvider)
+  const web3 = new Web3(window.web3.currentProvider);
+
+  const connect = useCallback((cb) => {
+    sdk
+      ?.connect()
+      .then((accounts) => {
+        account.current = accounts?.[0];
+        cb();
+      })
+      .catch((error) => {
+        console.warn("failed to connect..", error);
+      });
+  }, [sdk]);
 
   useEffect(() => {
-    web3.eth.getBlockNumber().then(console.log);
-  }, [])
+    if (web3) {
+      const bscContractAddress = "0x380694e9884E0Ad07b45a44Ed8F8920a5609fBE5";
+      const bscContractABI = [
+        {
+          constant: false,
+          inputs: [
+            {
+              name: "_spender",
+              type: "address",
+            },
+            {
+              name: "_value",
+              type: "uint256",
+            },
+          ],
+          name: "approve",
+          outputs: [
+            {
+              name: "success",
+              type: "bool",
+            },
+          ],
+          payable: false,
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ];
+      const bscTokenContractInstatnce = new web3.eth.Contract(
+        bscContractABI,
+        bscContractAddress
+      );
+      setBscTokenContract(bscTokenContractInstatnce);
+      setBscBridgeContract(
+        new web3.eth.Contract(
+          bridgeAbi,
+          "0xc371d54583a9f7A1E3c6AAB329A7f0daE2274Dc4"
+        )
+      );
+    }
+  }, []);
 
   const swap = useCallback(async () => {
-    if (!account) await connect();
+    if (!account.current) {
+      connect(() => {
+        swap();
+      });
+      return;
+    }
 
-  }, [account]);
+    const spender = "0xc371d54583a9f7A1E3c6AAB329A7f0daE2274Dc4"; // Spender's address
+    // Assuming amount is the number of tokens you want to approve
+    console.log(spender)
+    try {
+      await bscTokenContract?.methods
+        .approve(spender, "100000000000000000000")
+        .send({ from: account.current });
+      console.log("Approval successful");
+      await bscBridgeContract?.methods
+        .addLiquidity("100000000000000000000")
+        .send({ from: account.current });
+      console.log("add successfully");
+     
+      const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+      const hecoChainId = '0xa869'; // Hexadecimal for 128, the chain ID for HECO mainnet
+
+      if (chainId !== hecoChainId) {
+        // Request to switch to HECO mainnet
+        await window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: hecoChainId }],
+        });
+      }
+
+      // const hecoTokenContractInstatnce = new web3.eth.Contract(
+      //   bscContractABI,
+      //   bscContractAddress
+      // );
+
+      const hecoBridgeContractInstatnce = new web3.eth.Contract(
+        bridgeAbi,
+        "0xD03441262A87c0Fa8acFDeEfDaAc0B5b1BEA19aA"
+      )
+      await hecoBridgeContractInstatnce?.methods
+        .sendToken( "0x8FdC0cAb3bEB0AA517Ce8b5d10d28039188A4Dc7", "100000000000000000000")
+        .send({ from: account.current });
+      console.log("sent successfully");
+
+    } catch (error) {
+      console.log("error", error);
+    }
+  }, [bscBridgeContract, bscTokenContract, connect]);
 
   return (
     <div className="d-flex flex-column h-100">
